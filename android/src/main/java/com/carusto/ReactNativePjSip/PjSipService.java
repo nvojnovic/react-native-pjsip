e183a926c2033aa621b9fc875c742bd832961d71
Nenad Vojnovic <nvojnovic@zipato.com>
2019-11-28 09:32:47 +0100

    prevent android crash by properly destroying pjsip service

--- android/src/main/java/com/carusto/ReactNativePjSip/PjSipService.java
+++ android/src/main/java/com/carusto/ReactNativePjSip/PjSipService.java
@@ -263,7 +263,7 @@ public class PjSipService extends Service {
 
     @Override
     public void onDestroy() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+        if (mWorkerThread !== null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             mWorkerThread.quitSafely();
         }
 
@@ -272,6 +272,7 @@ public class PjSipService extends Service {
         try {
             if (mEndpoint != null) {
                 mEndpoint.libDestroy();
+                mEndpoint.delete();
             }
         } catch (Exception e) {
             Log.w(TAG, "Failed to destroy PjSip library", e);
